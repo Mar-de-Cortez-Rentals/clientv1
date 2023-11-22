9a18c23b065d4f79c0e9026b986a9313d7ec91de
NecroPrism4 <alexhid14@gmail.com>
2023-11-22 00:01:32 -0700

    a mimir

--- src/pages/Home/Dashboard/Dashboard.jsx
+++ src/pages/Home/Dashboard/Dashboard.jsx
@@ -1,35 +1,38 @@
-import { formatDistanceToNow, parseISO } from "date-fns";
-import { es } from "date-fns/locale";
-import { useContext, useEffect, useState } from "react";
-import { SectionContext } from "../../../context/SectionContext";
-import { useAuthContext } from "../../../hooks/useAuthContext";
-import { socket } from "../../../socket";
-import "./Dashboard.scss";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import es from 'date-fns/locale/es';
+import parseISO from 'date-fns/parseISO';
+import { useContext, useEffect, useState } from 'react';
+import { SectionContext } from '../../../context/SectionContext';
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { socket } from '../../../socket';
+import './Dashboard.scss';
+
+import { API_URL } from '../../../../config';
 
 function Dashboard() {
 	const MockData = () => {
 		const data = [
 			{
-				tenant: { first_name: "Juan", last_name: "Perez", phone: "1234567890", email: "" },
-				admin: { first_name: "Hermana ", last_name: "Galvez 1" },
+				tenant: { first_name: 'Juan', last_name: 'Perez', phone: '1234567890', email: '' },
+				admin: { first_name: 'Hermana ', last_name: 'Galvez 1' },
 				date: new Date(),
 				index: 0,
 			},
 			{
-				tenant: { first_name: "Lidia", last_name: "Susano", phone: "1234567890", email: "" },
-				admin: { first_name: "Hermana ", last_name: "Galvez 2" },
+				tenant: { first_name: 'Lidia', last_name: 'Susano', phone: '1234567890', email: '' },
+				admin: { first_name: 'Hermana ', last_name: 'Galvez 2' },
 				date: new Date(),
 				index: 1,
 			},
 			{
-				tenant: { first_name: "Frank", last_name: "CInco", phone: "1234567890", email: "" },
-				admin: { first_name: "Hermana ", last_name: "Galvez 3" },
+				tenant: { first_name: 'Frank', last_name: 'CInco', phone: '1234567890', email: '' },
+				admin: { first_name: 'Hermana ', last_name: 'Galvez 3' },
 				date: new Date(),
 				index: 2,
 			},
 			{
-				tenant: { first_name: "Tulio", last_name: "Palazueloz", phone: "1234567890", email: "" },
-				admin: { first_name: "Hermana ", last_name: "Galvez 4" },
+				tenant: { first_name: 'Tulio', last_name: 'Palazueloz', phone: '1234567890', email: '' },
+				admin: { first_name: 'Hermana ', last_name: 'Galvez 4' },
 				date: new Date(),
 				index: 3,
 			},
@@ -43,9 +46,9 @@ function Dashboard() {
 	//Handles the title for the upper navbar
 	const { handleTitle } = useContext(SectionContext);
 	useEffect(() => {
-		handleTitle("Bienvenid@");
+		handleTitle('Bienvenid@');
 
-		fetch("http://localhost:3000/property/0/4")
+		fetch(`${API_URL}/property/0/4`)
 			.then((res) => {
 				res.json().then((data) => {
 					setData(
@@ -65,7 +68,7 @@ function Dashboard() {
 	const { user } = useAuthContext();
 
 	const handleAddEvent = (value, index) => {
-		socket.emit("message", value);
+		socket.emit('message', value);
 	};
 
 	const [isConnected, setIsConnected] = useState(socket.connected);
@@ -87,14 +90,14 @@ function Dashboard() {
 			});
 		}
 
-		socket.on("connect", onConnect);
-		socket.on("disconnect", onDisconnect);
-		socket.on("message", onFooEvent);
+		socket.on('connect', onConnect);
+		socket.on('disconnect', onDisconnect);
+		socket.on('message', onFooEvent);
 
 		return () => {
-			socket.off("connect", onConnect);
-			socket.off("disconnect", onDisconnect);
-			socket.off("message", onFooEvent);
+			socket.off('connect', onConnect);
+			socket.off('disconnect', onDisconnect);
+			socket.off('message', onFooEvent);
 		};
 	}, []);
 
@@ -102,55 +105,17 @@ function Dashboard() {
 		<div className='HomeChildContainer Dashboard'>
 			<div className='DashboardContainer'>
 				<div className='ActionsContainer'>
-					{/* <div>
-						<Link className='RoundedRect ImageButton' to={"../inventory"}>
-							<button className='RoundedRect ImageButton'>
-								<div className='imageTextContainer'>
-									<img src='https://t3.ftcdn.net/jpg/01/03/74/84/360_F_103748444_vY2FIbRx86d969BLEMbCQ610Pkx6m3OX.jpg' />
-									<div>
-										<h4>{user?.user_type == "admin" ? "Ir a inventario" : "Registrar Prestamo"}</h4>
-									</div>
-								</div>
-							</button>
-						</Link>
-
-						<Link className='RoundedRect ImageButton' to={"../lendings"}>
-							<button className='RoundedRect ImageButton'>
-								<div className='imageTextContainer'>
-									<img
-										className='Image2'
-										src='https://s3-eu-central-1.amazonaws.com/eurosender-blog/wp-content/uploads/2017/04/19130136/returned-to-shipper-min.jpg'
-									/>
-									<div>
-										<h4>{user?.user_type == "admin" ? "Ir a prestamos" : "Devolución de prestamo"}</h4>
-									</div>
-								</div>
-							</button>
-						</Link>
-					</div> 
-
-					 <div className='SecondRow'>
-						<a className='RoundedRect ImageButton' href='https://www.tecnm.mx/'>
-							<img src='https://www.cdcuauhtemoc.tecnm.mx/wp-content/uploads/2021/08/Logo-TecNM.png' />
-						</a>
-						<a className='RoundedRect ImageButton' href='https://www.puertopenasco.tecnm.mx/'>
-							<img src='https://www.talent-network.org/comunidades/wp-content/uploads/2020/03/Instituto-Tecnologico-Superior-de-Puerto-Penasco.png' />
-						</a>
-						<a className='RoundedRect ImageButton' href='https://www.facebook.com/TecNMPuertoPenasco'>
-							<img src='https://privacyinternational.org/sites/default/files/styles/teaser_large_x1/public/2020-06/Facebook400x230.png.webp?itok=mRsS3Vli' />
-						</a>
-					</div>  */}
 					<div className='row'>
 						{fooEvents.map((item, index) => {
 							return (
-								<div className={`col-md-3 px-4 py-4 animateCard`} style={{ background: "var(--tertiary-bg)", borderRadius: "2rem" }} key={index}>
-									<div className='col-md pb-3'>
+								<div className={`col-md-3 px-4 pt-2 animateCard`} style={{ background: 'var(--tertiary-bg)', borderRadius: '2rem' }} key={index}>
+									<div className='col-md pb-3	'>
 										<div className='row'>
 											<img
 												src={MockImages()}
 												className='col-md shadow p-0'
 												alt='...'
-												style={{ borderRadius: "1rem", height: "100px", objectFit: "cover" }}
+												style={{ borderRadius: '1rem', height: '100px', objectFit: 'cover', width: '100%' }}
 											/>
 											<div className='col-md pt-3'>
 												<h5 className='card-title'>
@@ -160,30 +125,31 @@ function Dashboard() {
 										</div>
 									</div>
 
-									<div className='col-md'>
-										<div className='row'>
-											<div className='col-md'>
-												<h5 className='card-title'>{item.property.name}</h5>
-												<p className='card-text'>{item.property.address}</p>
-											</div>
+									<div className='d-flex w-100 flex-column'>
+										<div className=''>
+											<h5 className='card-title'>{item.property.name}</h5>
+											<p className='card-text'>
+												<span>Dirección: </span>
+												{item.property.address}
+											</p>
+										</div>
 
-											<div className='col-md'>
-												<h5 className='card-title'>
-													Recibio: {item.admin.first_name} {item.admin.last_name}
-												</h5>
-											</div>
+										<div className=''>
+											<h5 className='card-title'>
+												Recibio: {item.admin.first_name} {item.admin.last_name}
+											</h5>
+										</div>
 
-											<div className='col-md'>
-												<h5 className='card-title'>
-													Fecha:{" "}
-													{formatDistanceToNow(parseISO(item.date), {
-														addSuffix: true,
-														...{
-															locale: es,
-														},
-													})}
-												</h5>
-											</div>
+										<div className=''>
+											<h5 className='card-title'>
+												Fecha:{' '}
+												{formatDistanceToNow(parseISO(item.date), {
+													addSuffix: true,
+													...{
+														locale: es,
+													},
+												})}
+											</h5>
 										</div>
 									</div>
 								</div>
@@ -201,19 +167,19 @@ function Dashboard() {
 						{data.map((item, index) => {
 							console.log(item);
 							return (
-								<div className='col-md-3' key={index}>
-									<div className={`card shadow mockPendingPatCard animateCard`}>
+								<div className='col-md-3 mb-3' key={index}>
+									<div className={`card shadow mockPendingPatCard animateCard`} style={{ border: 'none', background: 'var(--tertiary-bg)' }}>
 										<img src={MockImages()} className='card-img-top' alt='...' />
-										<div className='card-body'>
+										<div className='card-body' style={{ background: 'transparent' }}>
 											<h5 className='card-title'>{item.property.name}</h5>
 											<p className='card-text'>{item.property.address}</p>
 											<a
 												className='btn btn-primary'
-												style={{ borderRadius: "1rem" }}
+												style={{ borderRadius: '1rem' }}
 												onClick={() => {
 													handleAddEvent(item, index);
 												}}>
-												<p className='text' style={{ color: "var(--tertiary-bg)" }}>
+												<p className='text' style={{ color: 'var(--tertiary-bg)' }}>
 													Confirmar pago
 												</p>
 											</a>
@@ -233,10 +199,10 @@ export default Dashboard;
 
 const MockImages = () => {
 	const data = [
-		"https://img10.naventcdn.com/avisos/18/00/90/75/26/43/720x532/1104741056.jpg",
-		"https://http2.mlstatic.com/D_NQ_NP_786367-MLM69746404598_062023-O.webp",
-		"https://gpvivienda.com/blog/wp-content/uploads/2023/03/ralph-ravi-kayden-mR1CIDduGLc-unsplash-1-1-1024x680.jpg",
-		"https://fincaraiz.com.co/blog/wp-content/uploads/2022/08/casas-modernas-1-1920x1130.jpg",
+		'https://img10.naventcdn.com/avisos/18/00/90/75/26/43/720x532/1104741056.jpg',
+		'https://http2.mlstatic.com/D_NQ_NP_786367-MLM69746404598_062023-O.webp',
+		'https://gpvivienda.com/blog/wp-content/uploads/2023/03/ralph-ravi-kayden-mR1CIDduGLc-unsplash-1-1-1024x680.jpg',
+		'https://fincaraiz.com.co/blog/wp-content/uploads/2022/08/casas-modernas-1-1920x1130.jpg',
 	];
 
 	return data[Math.floor(Math.random() * data.length)];
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
+import { API_URL } from '../config';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:2000";
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:2000';
 
-export const socket = io("http://localhost:3000");
+export const socket = io(API_URL);
